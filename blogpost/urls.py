900b4d360ce29dddbf55724cc0faab62a5063a3d
amrit <amritpra94@gmail.com>
2022-01-16 16:45:04 +0530

    media file access

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
     path('', include('article.urls')),
     path('api/', include('article.api_urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
